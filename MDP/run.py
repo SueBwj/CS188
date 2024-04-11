13630f24ae9ba252b618ef3ff8fe1390551ce939
Sue <1175634156@qq.com>
2024-04-11 10:41:09 +0800

    增改一些文件

--- MDP/run.py
+++
@@ -1,7 +0,0 @@
-from GridWorld import GridWorld
-from Grid import Grid
-import numpy as np
-grid = Grid()
-grid_world = GridWorld(grid=grid,goalVals=np.arange(10))
-grid_world.simulate(np.arange(10))
-
